c585f7bb3527448acf99d78f6c5bd66839121f76
RafatMeraz <razhossain201@gmail.com>
2023-07-21 23:01:00 +0600

    water track models has been added

---
+++ models/water_track.go
@@ -0,0 +1,28 @@
+package models
+
+import "gorm.io/gorm"
+
+type WaterTrack struct {
+	ID          uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+	UserId      uint           `json:"user_id" gorm:"index"`
+	Amount      uint           `json:"amount" gorm:"default:0"`
+	ConsumeTime uint           `json:"consume_time"`
+	CreateAt    uint           `json:"create_at" gorm:"autoCreateTime:milli"`
+	UpdatedAt   uint           `json:"updated_at" gorm:"autoUpdateTime:milli"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
+}
+
+type WaterTrackRequest struct {
+	UserId      uint `json:"user_id" validate:"required"`
+	Amount      uint `json:"amount" validate:"required"`
+	ConsumeTime uint `json:"consume_time" validate:"required"`
+}
+
+type WaterTrackDTO struct {
+	ID          uint `json:"id"`
+	UserId      uint `json:"user_id"`
+	Amount      uint `json:"amount"`
+	ConsumeTime uint `json:"consume_time"`
+	CreatedAt   uint `json:"created_at"`
+	UpdatedAt   uint `json:"updated_at"`
+}
