4537a97d75a06ab2fb468cb61a488e714195dc1d
RafatMeraz <razhossain201@gmail.com>
2023-09-09 11:09:07 +0600

    configuration files has been removed

--- controllers/jwt_token_controller.go
+++ controllers/jwt_token_controller.go
@@ -1,16 +1,18 @@
 package controllers
 
 import (
-	"github.com/RafatMeraz/h20/config"
 	"github.com/RafatMeraz/h20/models"
 	"github.com/golang-jwt/jwt"
+	"os"
+	"strconv"
 	"time"
 )
 
 type JWTTokenController struct{}
 
 func (JWTTokenController) CreateJwtToken(userId uint) (string, int64, error) {
-	expiryTime := time.Now().Add(config.AppConfiguration.TokenValidationTime) // expire time of token
+	hours, _ := strconv.Atoi(os.Getenv("TOKEN_VALIDATION_TIME"))
+	expiryTime := time.Now().Add(time.Hour * time.Duration(hours)) // expire time of token
 	claims := models.JwtCustomClaims{
 		UserId: userId,
 		StandardClaims: jwt.StandardClaims{
@@ -19,7 +21,7 @@ func (JWTTokenController) CreateJwtToken(userId uint) (string, int64, error) {
 	}
 
 	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	token, err := tokenClaims.SignedString(config.AppConfiguration.TokenSecret)
+	token, err := tokenClaims.SignedString([]byte(os.Getenv("TOKEN_SECRET")))
 	if err != nil {
 		return "", 0, err
 	}
@@ -38,7 +40,7 @@ func (JWTTokenController) CheckTokenValidation(claims *models.JwtCustomClaims) b
 func (JWTTokenController) GetClaimsFromToken(strToken string) (models.JwtCustomClaims, error) {
 	var claims models.JwtCustomClaims
 	_, err := jwt.ParseWithClaims(strToken, &claims, func(token *jwt.Token) (interface{}, error) {
-		return config.AppConfiguration.TokenSecret, nil
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
 	})
 	if err != nil {
 		return models.JwtCustomClaims{}, err
